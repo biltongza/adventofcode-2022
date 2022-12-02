afea1d2d6c4ae9f42c70517326f323ca2798ccb5
Logan Dam <dam.loganza@gmail.com>
2022-12-02 15:23:12 +0100

    day 2 part 2

--- day2/day2.go
+++ day2/day2.go
@@ -21,7 +21,6 @@ func Day2() {
 	}
 	scanner := bufio.NewScanner(file)
 
-	totalOpponentScore := 0
 	totalMyScore := 0
 	for scanner.Scan() {
 		line := scanner.Text()
@@ -32,36 +31,45 @@ func Day2() {
 		roundOpponentScore := 0
 		roundMyScore := 0
 		opponentMove := parts[0]
-		myMove := parts[1]
+		myTargetMove := parts[1]
 
 		opponentScore := scores[opponentMove]
-		roundOpponentScore += opponentScore
-		myScore := scores[myMove]
-		roundMyScore += myScore
 
-		opponentResult, myResult := determineWin(opponentScore, myScore)
+		opponentResult, myResult, myMove := determineScores(opponentScore, myTargetMove)
 
 		roundOpponentScore += opponentResult
-		roundMyScore += myResult
-		totalOpponentScore += roundOpponentScore
+		roundMyScore += myResult + myMove
 		totalMyScore += roundMyScore
 
-		fmt.Fprintln(os.Stdout, opponentMove, myMove, opponentScore, myScore, opponentResult, myResult, roundOpponentScore, roundMyScore)
 	}
-	fmt.Fprintln(os.Stdout, "opponent total score:", totalOpponentScore)
 	fmt.Fprintln(os.Stdout, "my total score: ", totalMyScore)
 }
 
-func determineWin(player1 int, player2 int) (player1Score int, player2Score int) {
-	if (player1 == 1 && player2 == 3) || player1-player2 == 1 {
-		player1Score = 6
-		player2Score = 0
-	} else if (player1 == 3 && player2 == 1) || player2-player1 == 1 {
-		player1Score = 0
-		player2Score = 6
-	} else {
-		player1Score = 3
-		player2Score = 3
+func determineScores(opponentMove int, myTargetMove string) (opponentScore int, myScore int, myMove int) {
+	if myTargetMove == "X" {
+		opponentScore = 6
+		myScore = 0
+		if opponentMove == 1 {
+			myMove = 3
+		} else if opponentMove == 2 {
+			myMove = 1
+		} else if opponentMove == 3 {
+			myMove = 2
+		}
+	} else if myTargetMove == "Y" {
+		opponentScore = 3
+		myScore = 3
+		myMove = opponentMove
+	} else if myTargetMove == "Z" {
+		opponentScore = 0
+		myScore = 6
+		if opponentMove == 1 {
+			myMove = 2
+		} else if opponentMove == 2 {
+			myMove = 3
+		} else if opponentMove == 3 {
+			myMove = 1
+		}
 	}
 	return
 }
