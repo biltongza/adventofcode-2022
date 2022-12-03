b1d734dd9b2b27095a8788a87084539c4de563a9
Logan Dam <dam.loganza@gmail.com>
2022-12-03 16:01:31 +0100

    day 3 part 2

--- day3/day3.go
+++ day3/day3.go
@@ -1,17 +1,19 @@
 package day3
 
 import (
-	"bufio"
 	"fmt"
 	"os"
+	"strings"
 )
 
 func Day3() {
-	file, err := os.Open("day3/input.txt")
+	file, err := os.ReadFile("day3/input.txt")
 	if err != nil {
 		panic(err)
 	}
-	scanner := bufio.NewScanner(file)
+	fileAsString := string(file)
+	lines := strings.SplitAfter(fileAsString, "\n")
+
 	priorities := make(map[rune]int)
 	for i := 0; i <= 26; i++ {
 		priorities[rune(int('a')+i)] = i + 1
@@ -21,21 +23,26 @@ func Day3() {
 	}
 
 	total := 0
-	for scanner.Scan() {
-		line := scanner.Text()
-		if line == "" {
+	for lineIndex := 0; lineIndex <= len(lines); {
+		firstElf := lines[lineIndex]
+		if firstElf == "" {
 			break
 		}
-		lineLength := len(line)
-		firstCompartment := line[0 : lineLength/2]
-		secondCompartment := line[lineLength/2 : lineLength]
+		secondElf := lines[lineIndex+1]
+		thirdElf := lines[lineIndex+2]
+
 		var item rune
-		for _, itemFirstCompartment := range firstCompartment {
+		for _, itemFirstElf := range firstElf {
 			found := false
-			for _, itemSecondCompartment := range secondCompartment {
-				if itemFirstCompartment == itemSecondCompartment {
-					found = true
-					item = itemFirstCompartment
+			for _, itemSecondElf := range secondElf {
+				for _, itemThirdElf := range thirdElf {
+					if itemFirstElf == itemSecondElf && itemFirstElf == itemThirdElf {
+						found = true
+						item = itemFirstElf
+						break
+					}
+				}
+				if found {
 					break
 				}
 			}
@@ -45,6 +52,7 @@ func Day3() {
 		}
 
 		total += priorities[item]
+		lineIndex += 3
 	}
 	fmt.Fprintln(os.Stdout, "total", total)
 }
