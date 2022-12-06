781e1eff6779eac6b18caf94078c8d40f6c74e2d
Logan Dam <dam.loganza@gmail.com>
2022-12-06 16:13:34 +0100

    day 6 part 2

--- day6/day6.go
+++ day6/day6.go
@@ -18,32 +18,38 @@ func Day6() {
 		if line == "" {
 			break
 		}
-		length := len(line)
-		for i := 0; i <= length; i++ {
-			buf := line[i : i+4]
-			skip := false
-			for index, char := range buf {
-				for j := 0; j <= 3; j++ {
-					if j <= index {
-						continue
-					}
-					otherChar := rune(buf[j])
-					if char == otherChar {
-						skip = true
-						break
-					}
+		startOfMessage := detectUniqueSequence(line, 14)
+		fmt.Fprintf(os.Stdout, "detected start of message at %d\n", startOfMessage)
+	}
+}
+
+func detectUniqueSequence(str string, sequenceLength int) int {
+	length := len(str)
+	num := -1
+	for i := 0; i <= length; i++ {
+		buf := str[i : i+sequenceLength]
+		skip := false
+		for index, char := range buf {
+			for j := 0; j <= sequenceLength-1; j++ {
+				if j <= index {
+					continue
 				}
-				if skip {
+				otherChar := rune(buf[j])
+				if char == otherChar {
+					skip = true
 					break
 				}
 			}
 			if skip {
-				continue
-			} else {
-				fmt.Fprintf(os.Stdout, "found start of packet at index %d\n", i+4)
 				break
 			}
 		}
-
+		if skip {
+			continue
+		} else {
+			num = i + sequenceLength
+			break
+		}
 	}
+	return num
 }
